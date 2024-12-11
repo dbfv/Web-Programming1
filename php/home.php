bc2392fa16cbd580c4be3bae702b8d93e7e3890e
dbfv <quanbfclan@gmail.com>
2024-12-11 14:22:18 +0700

    ORDER BY post_date

--- php/home.php
+++ php/home.php
@@ -1,11 +1,14 @@
 <?php 
-// require_once "../includes/authorization.php";
+require_once "../includes/authorization.php";
 
 $title = "Home";
 ob_start();
 
 include '../includes/connect_db.php';
-$sql = "SELECT posts.*, users.* FROM posts INNER JOIN users ON posts.user_id = users.id;";
+$sql = "SELECT posts.*, users.*
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+ORDER BY posts.post_date DESC;";
 $posts = $pdo->query($sql);
 $posts = $posts->fetchAll(PDO::FETCH_ASSOC);
 
