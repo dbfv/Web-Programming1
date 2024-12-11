7bb4d698d3a81dc739e3eb2d3c80f9365c7b687f
dbfv <quanbfclan@gmail.com>
2024-12-11 13:55:15 +0700

    add check if empty

--- includes/queries/users.php
+++ includes/queries/users.php
@@ -3,3 +3,6 @@ require '../includes/connect_db.php';
 // Retrieve data of users from the database
 $sql = 'SELECT * FROM users'; 
 $users = $pdo->query($sql);
+if (!$users) {
+    die("Query failed.");
+}
